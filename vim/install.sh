d2036a05e704c1427822079509ec80fbaa0f9dea
Matt Smith <matt@smith.codes>
2025-11-10 04:33:42 -0600

    : instead / :)

--- vim/install.sh
+++ vim/install.sh
@@ -2,4 +2,4 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 rm -fr ~/.vim/bundle/vim-colors-solarized
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+git clone git://github.com:altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
